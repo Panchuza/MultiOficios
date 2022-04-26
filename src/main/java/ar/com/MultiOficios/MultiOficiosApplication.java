fd59ea3d7bfde4bf1d60bb912cb030aecacb9e37
Francisco Arana <panchoarana2014@gmail.com>
2022-04-25 21:49:34 -0300

    Comienzo Proyecto Final

---
+++ src/main/java/ar/com/MultiOficios/MultiOficiosApplication.java
@@ -0,0 +1,13 @@
+package ar.com.MultiOficios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiOficiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiOficiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/MultiOficios/MultiOficiosApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.MultiOficios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiOficiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
