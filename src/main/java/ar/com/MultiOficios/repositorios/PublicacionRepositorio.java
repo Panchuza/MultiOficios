e3d9074ed44e98bb71c8a50d70bcb479c51ca5f7
WuilmanA <wuilmanarias1970@gmail.com>
2022-04-27 22:05:04 -0300

    Creacion de Servicio Publicacion

--- src/main/java/ar/com/MultiOficios/repositorios/PublicacionRepositorio.java
+++ src/main/java/ar/com/MultiOficios/repositorios/PublicacionRepositorio.java
@@ -3,9 +3,16 @@ package ar.com.MultiOficios.repositorios;
 
 import ar.com.MultiOficios.entidades.Publicacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PublicacionRepositorio extends JpaRepository<Publicacion, String>{
     
+    @Query("SELECT u FROM Publicacion u WHERE u.nombre LIKE :nombre")
+    public Publicacion buscarPorNombre(@Param("nombre") String nombre);
+    
+    @Query("SELECT u FROM Publicacion u")
+    public Publicacion ListarPublicacion();
 }
