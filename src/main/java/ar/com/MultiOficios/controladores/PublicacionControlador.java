ae4aa084a3c0b05d08685b790970dc96d75d6d46
WuilmanA <wuilmanarias1970@gmail.com>
2022-05-10 22:28:16 -0300

    modificacion controlador y HTML Prueba Publicacion

--- src/main/java/ar/com/MultiOficios/controladores/PublicacionControlador.java
+++ src/main/java/ar/com/MultiOficios/controladores/PublicacionControlador.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -17,6 +19,10 @@ public class PublicacionControlador {
     @Autowired
     private PublicacionServicio publicacionServicio;
     
+     @GetMapping("/administrarPublicacion")
+    public String administrarPublicacion() {
+        return "administrarPublicacion.html";
+    }
     
      //*********Levanta las paginas listadoras****************
     @GetMapping("/listarPublicaciones")
@@ -24,5 +30,21 @@ public class PublicacionControlador {
         List<Publicacion> publicaciones = publicacionServicio.listarPublicaciones();
         modelo.put("publicaciones", publicaciones);
         return "listarPublicaciones.html";
+    }
+    
+    
+    @PostMapping("/administrarPublicacion")
+    public String crearPublicacion(ModelMap modelo, @RequestParam String nombre, @RequestParam String descripcion) throws ErrorServicio{
+
+        try {
+
+            publicacionServicio.crear(nombre,descripcion);
+            modelo.put("exito", "La publicacion" + nombre + " Se cargo exitosamente");
+            
+            return "listarPublicaciones.html";
+        } catch (ErrorServicio ex) {
+            modelo.put("ErrorServicio", ex.getMessage());
+            return "administrarPublicacion.html";
+        }
     }
 }
