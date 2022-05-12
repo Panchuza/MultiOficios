ac5ec593eda63d4df24c1d4ede1d583ef23eb840
joako <joako@carreralinux.com.ar>
2022-05-11 21:22:04 -0300

    avance zonaControlador y vista perfil

--- src/main/java/ar/com/MultiOficios/controladores/PortalControlador.java
+++ src/main/java/ar/com/MultiOficios/controladores/PortalControlador.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PortalControlador {
 
     
-//    @GetMapping("/login")
-//    public String login() {
-//        return "login.html";
+//    @GetMapping("/perfil")
+//    public String perfil() {
+//        return "perfil.html";
 //    }
 //    
 //    @GetMapping("/")
-//    public String pruebaRol(){
-//        return "/login";
+//    public String perfil(){
+//        return "/perfil";
 //    }
 }
--- src/main/java/ar/com/MultiOficios/controladores/ZonaControlador.java
+++ src/main/java/ar/com/MultiOficios/controladores/ZonaControlador.java
@@ -40,4 +40,8 @@ public class ZonaControlador {
         return "redirect:/zona/formZona";
     }
 
+    @GetMapping("perfil")
+    public String perfil() {
+        return "perfil.html";
+    }
 }
